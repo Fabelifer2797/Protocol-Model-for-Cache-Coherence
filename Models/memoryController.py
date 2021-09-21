8e759cb5f515b6e50fdff5da1acf67198cb39735
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2021-09-21 16:57:15 -0600

    Add memory controller model

--- Models/memoryController.py
+++ Models/memoryController.py
@@ -1,5 +1,69 @@
 class MemoryController:
 
-    def __init__(self):
+    directory = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
-        self.holamundo = "hola mundo"
+    def __init__(self, _currentCPU):
+
+        self.currentCPU = _currentCPU
+
+    def getCurrentCPU(self):
+
+        return self.currentCPU
+
+    def writeBackMemory(self, currentInstruction, myL1Cache, _mainMemory): return 0
+
+    def getCacheBlock1_way(self, dirMem):
+
+        return dirMem % 4
+
+    def generateTag(self, dirMem):
+
+        if dirMem <= 3:
+
+            return 0
+
+        else:
+
+            return 1
+
+    def checkCacheBlock(self, dirMem, cacheTag, cacheDir):
+
+        finalCacheDir = format(cacheTag,'02b') + format(cacheDir,'02b')
+
+        return format(dirMem,'04b') == finalCacheDir
+
+    def updateDirectory(self, dirMem, processorID, value):
+
+        MemoryController.directory[dirMem][processorID] = value
+
+    def getCurrentState(self, cacheBlock): return 0
+
+    def toStringDirectory(self):
+
+        print("Directory Values: ")
+        counter = 0
+
+        for block in MemoryController.directory:
+
+            print("Block {}: |".format(counter), end = " ")
+
+            for bit in block:
+
+                print(bit, "  |  ", end = "")
+
+            print("")
+            counter += 1
+
+def mainMemoryController():
+
+    memoryController1 = MemoryController(2)
+    memoryController1.updateDirectory(7,2,1)
+    memoryController1.toStringDirectory()
+    print(memoryController1.checkCacheBlock(7,0,3))
+
+
+if __name__ == "__main__":
+
+    print("Memory Controller Class!")
+    mainMemoryController()
+        
